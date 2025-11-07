636ff880944c95ff0a4a248e66e13a1628259df2
unknown <tugashafwan@gmail.com>
2025-11-07 11:25:12 +0700

    read produk admin

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\AdminProduct;
 class AdminProductController extends Controller
 {
     public function index()
     {
-        return view('admin.dataProduk');
+        $products = AdminProduct::orderBy('updated_at', 'desc')->paginate(10); // 10 item per page
+        return view('admin.dataProduk', compact('products'));
     }
 
     public function create()
