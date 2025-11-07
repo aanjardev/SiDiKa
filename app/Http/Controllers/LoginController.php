a3d161defe1524eca84b3134c95aebd124ecfafc
a-ali-wafa <aliwafa3575@gmail.com>
2025-11-07 09:30:34 +0700

    fixing-route

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('/admin');
+            return redirect()->intended('/admin/dashboard');
         }
 
         // Jika login gagal
