99645a5cb5604d7f16eb0ed738fa0eb0f6b22198
qurrotulaini0325 <qurrotulaini0325@gmail.com>
2025-12-02 23:24:55 +0700

    vite lagi

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/public/build/',
     plugins: [
         laravel({
             input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                // Page entries
-                "resources/js/penjualan/penjualan.js",
-                
-                "resources/js/pembelian/pembelian.js",
-                "resources/js/qualityControl/data-qc.js",
-                // Standalone utilities referenced directly via @vite in Blade
-                "resources/js/utils/clickable-rows.js",
-                "resources/js/utils/handle-delete.js",
-                "resources/js/utils/search.js",
-                "resources/js/utils/phone-input-validation.js",
-
-                // == ASET ADMIN (BARU) ==
-                // CSS Admin
-                "resources/admin_theme/css/core/libs.min.css",
-                "resources/admin_theme/vendor/aos/dist/aos.css",
-                "resources/admin_theme/css/hope-ui.min.css",
-                "resources/admin_theme/css/custom.min.css",
-                "resources/admin_theme/css/dark.min.css",
-                "resources/admin_theme/css/customizer.min.css",
-                "resources/admin_theme/css/rtl.min.css",
-
-                // // JS Admin
-                // "resources/admin_theme/js/core/libs.min.js",
-                // "resources/admin_theme/js/core/external.min.js",
-                // // "resources/admin_theme/js/plugins/setting.js",
-                // "resources/admin_theme/js/hope-ui.js",
+                'resources/css/app.css',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
