305f53c6993ca30582803e05981f2517a080d02e
farelfhr <farelfathir008@gmail.com>
2025-12-06 02:32:59 +0700

    yessir

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -1,26 +1,26 @@
 <?php
 
-namespace Tests\Browser;
+// namespace Tests\Browser;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Sleep;
-use Laravel\Dusk\Browser;
-use Tests\DuskTestCase;
+// use Illuminate\Foundation\Testing\DatabaseMigrations;
+// use Illuminate\Support\Sleep;
+// use Laravel\Dusk\Browser;
+// use Tests\DuskTestCase;
 
-class LoginTest extends DuskTestCase
-{
-    /**
-     * A Dusk test example.
-     */
-    public function testExample(): void
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/login')
-                    ->assertSee('SiDiKa')->type('email','admin@dinoyokamera.com')->type('password','admin123')->click('@btn-sign-in')
-                    ->assertSee('Dashboard');
-                    Sleep::for(10)->seconds();
-        });
+// class LoginTest extends DuskTestCase
+// {
+//     /**
+//      * A Dusk test example.
+//      */
+//     public function testExample(): void
+//     {
+//         $this->browse(function (Browser $browser) {
+//             $browser->visit('/login')
+//                     ->assertSee('SiDiKa')->type('email','admin@dinoyokamera.com')->type('password','admin123')->click('@btn-sign-in')
+//                     ->assertSee('Dashboard');
+//                     Sleep::for(10)->seconds();
+//         });
 
         
-    }
-}
+//     }
+// }
